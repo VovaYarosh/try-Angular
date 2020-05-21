09c34129ed493f731dcc9443615520bb9d33c152
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-21 12:05:15 +0300

    search pipe for list filtration

--- Shop/src/app/admin/admin.module.ts
+++ Shop/src/app/admin/admin.module.ts
@@ -10,6 +10,7 @@ import { OrdersPageComponent } from './orders-page/orders-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AuthGuard} from '../shared/auth.guard';
 import {QuillModule} from 'ngx-quill';
+import {SearchPipe} from '../shared/search.pipe';
 
 @NgModule({
   imports: [
@@ -29,7 +30,8 @@ import {QuillModule} from 'ngx-quill';
     }])
   ],
   exports: [RouterModule],
-  declarations: [AdminLayoutComponent, LoginPageComponent, AddPageComponent, DashboardPageComponent, EditPageComponent, OrdersPageComponent]
+  declarations: [
+    AdminLayoutComponent, LoginPageComponent, AddPageComponent, DashboardPageComponent, EditPageComponent, OrdersPageComponent, SearchPipe]
 })
 
 export class AdminModule {
