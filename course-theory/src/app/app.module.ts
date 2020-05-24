09195382213bf03119fbd83eb14d776bc55e37bb
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-24 19:58:03 +0300

    one-way binding

--- course-theory/src/app/app.module.ts
+++ course-theory/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule,
