5b7f0a5e8cafb7184e1ee05b44a2554e962c9c03
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-24 21:43:37 +0300

    two-ways binding

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -6,13 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  inputValue = '';
-  text = '';
-  constructor(){}
-  onInput(event){
-    this.inputValue = event.target.value;
-  }
-  onClick(value){
-    this.text = value;
+  title = '';
+  onInput(event: any){
+    this.title = event.target.value;
   }
 }
--- course-theory/src/app/app.module.ts
+++ course-theory/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PostComponent} from './post/post.component';
 import { Post2Component } from './post2/post2.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Post2Component } from './post2/post2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
