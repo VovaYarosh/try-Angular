0ad4472ab7c75a8afa17e613287e85c12966f943
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-25 22:07:00 +0300

    directive ngStyle

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = '';
-  onInput(event: any){
-    this.title = event.target.value;
-  }
+  backGroundToggle = false;
 }
