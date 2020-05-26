fb4d8c5dc58ac5b583feb746083bf4ed7b5ebad6
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-26 16:20:26 +0300

    pipe introduction

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  arr = [1, 1, 2, 3, 5, 8, 13];
-  objs = [
-    {title: 'Post 1', author: 'Vova', comments: [
-        {name: 'Bob', text: 'lorem 1'},
-        {name: 'Carl', text: 'lorem 2'},
-        {name: 'Dean', text: 'lorem 3'},
-      ]},
-    {title: 'Post 2', author: 'Vova', comments: [
-        {name: 'Bob', text: 'lorem 1'},
-        {name: 'Carl', text: 'lorem 2'},
-        {name: 'Dean', text: 'lorem 3'},
-      ]},
-  ];
+ now: Date = new Date();
 }
