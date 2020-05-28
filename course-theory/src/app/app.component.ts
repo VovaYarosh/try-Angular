c7d7153be1d1679becca507a6823f3e63b40b34d
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-05-28 23:40:47 +0300

    passing parametres @input

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    {title: 'Wanna learn angular components', text: 'I am still learning components yet', id: 1},
+    {title: 'Next block', text: 'Will be about directives and about pipes', id: 1}
+  ];
 }
