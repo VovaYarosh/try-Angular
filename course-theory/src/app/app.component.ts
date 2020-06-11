8ce814d01cf32ba5eabb517311f96a61c90f8046
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-06-11 20:02:29 +0300

    how to make directive introduction

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -1,31 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  posts: Post[] = [
-    {title: 'Wanna learn angular components', text: 'I am still learning components yet', id: 1},
-    // {title: 'Next block', text: 'Will be about directives and about pipes', id: 1}
-  ];
-  ngOnInit(): void {
-    setTimeout( () => {
-      console.log('timeout');
-      this.posts[0] = {title: 'changed', text: 'cheng detection startegy', id: 3};
-  }, 5000);
-  }
-  updatePosts(post){
-    this.posts.unshift(post);
-  }
-  removePost(id: number){
-    this.posts = this.posts.filter(p => p.id !== id);
-  }
+export class AppComponent{
+
 }
--- course-theory/src/app/app.module.ts
+++ course-theory/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import {PostComponent} from './post/post.component';
+import {StyleDirective} from './directives/style.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent
+    StyleDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
