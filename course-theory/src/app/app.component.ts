19f26d0049b24e8d1f2cfd1ad1421e1db009a4f2
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-09 14:37:24 +0300

    list filter by pipe

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -1,23 +1,20 @@
 import {Component} from '@angular/core';
 
+export interface  Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
-  e: number = Math.E;
-  str = 'hello world';
-  date: Date = new Date;
-  float = 0.42;
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4
-      }
-    }
-  };
+  search = '';
+  posts: Post[] = [
+    {title: 'Beer', text: 'the best beer in UK'},
+    {title: 'JS', text: 'the best leanguage in the world'},
+    {title: 'Bread', text: 'the best bread in Ukraine'}
+  ];
 }
