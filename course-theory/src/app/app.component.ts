ef9686d9b73a239a7edf1ca6ca129cbd1baf7510
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-11 17:54:39 +0300

    forms creating controls

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +9,14 @@ import {FormGroup} from '@angular/forms';
 export class AppComponent implements OnInit{
   form: FormGroup;
   ngOnInit(){
-    this.form = new FormGroup({});
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null),
+    });
   }
   submit(){
-    console.log('form submitted: ', this.form);
+    console.log('Form: ', this.form)
+    const formData = {...this.form.value};
+    console.log('form data:', formData);
   }
 }
