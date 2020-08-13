323507e5586287482972be0e388c88c82520efb2
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-13 18:01:07 +0300

    forms creating a group

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -11,13 +11,21 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.form = new FormGroup({
       email: new FormControl('', [
-        Validators.email, Validators.required]),
+        Validators.email,
+        Validators.required
+      ]),
       password: new FormControl(null, [
-        Validators.minLength(6), Validators.required]),
+        Validators.minLength(6),
+        Validators.required
+      ]),
+      address: new FormControl({
+        country: new FormControl('ua'),
+        city: new FormControl('', Validators.required)
+      })
     });
   }
   submit(){
-    console.log('Form: ', this.form)
+    console.log('Form: ', this.form);
     const formData = {...this.form.value};
     console.log('form data:', formData);
   }
