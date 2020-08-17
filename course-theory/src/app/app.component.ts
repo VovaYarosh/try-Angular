630a029f5a43f734d320b66e7cc632df79837bd4
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-17 07:05:52 +0300

    httpClient introducing

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  appState = 'on';
-  handleChange(){
-    console.log(this.appState);
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+
   }
 }
+
