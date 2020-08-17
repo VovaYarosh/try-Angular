14871d2a9d1c559eef34886a1c0ab81bcf4749c5
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-17 08:10:43 +0300

    httpClient creating objects with POST

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -14,11 +14,27 @@ export  interface Todo {
 })
 export class AppComponent implements OnInit {
   todos: Todo[] = [];
+  todoTitle = '';
   constructor(public http: HttpClient){}
   ngOnInit() {
     this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe(todos => {
         this.todos = todos;
+      });
+  }
+  addTodo() {
+    if (this.todoTitle.trim()){
+      return;
+    }
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log('todo', todo);
+        this.todos.push(todo);
+        this.todoTitle = '';
       });
   }
 }
