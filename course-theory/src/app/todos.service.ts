fa12faa1ecabc0168fee2f559b6aabb29242f615
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-18 17:16:24 +0300

    httpClient adding headers

--- course-theory/src/app/todos.service.ts
+++ course-theory/src/app/todos.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError, delay} from 'rxjs/operators';
 import {error} from 'selenium-webdriver';
@@ -14,7 +14,12 @@ export  interface Todo {
 export class TodosService {
   constructor(private http: HttpClient) {}
   addTodo(todo: Todo): Observable<Todo>{
-    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo);
+    const headers = new HttpHeaders({
+      'MyCustomHeader': 'go'
+    })
+    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
+      headers
+    });
   }
   fetchTodos(): Observable<Todo[]>{
     return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
