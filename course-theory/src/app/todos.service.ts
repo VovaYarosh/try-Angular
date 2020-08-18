bfd7f01da281b836172f9eae1d46839614e12ea5
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-18 18:04:37 +0300

    httpClient work with params

--- course-theory/src/app/todos.service.ts
+++ course-theory/src/app/todos.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError, delay} from 'rxjs/operators';
 import {error} from 'selenium-webdriver';
@@ -16,13 +16,18 @@ export class TodosService {
   addTodo(todo: Todo): Observable<Todo>{
     const headers = new HttpHeaders({
       'MyCustomHeader': 'go'
-    })
+    });
     return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
       headers
     });
   }
   fetchTodos(): Observable<Todo[]>{
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    let params = new HttpParams();
+    params = params.append('_limit', '4');
+    params = params.append('coustom', 'anything');
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2', {
+      params
+    })
       .pipe(delay(500),
         catchError( error => {
           console.log('error: ', error.message);
