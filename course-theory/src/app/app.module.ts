34acf174870ceb1b1d15803cd5ffd2c52322b79d
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-08-21 15:29:38 +0300

    dynamic components

--- course-theory/src/app/app.module.ts
+++ course-theory/src/app/app.module.ts
@@ -3,24 +3,18 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {HomePageComponent} from './home-page/home-page.component';
-import {AppRoutingModule} from './app-routing.module';
-import {AboutPageComponent} from './about-page/about-page.component';
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component';
-import {SharedModule} from './shared/shared.module';
+import { ModalComponent } from './modal/modal.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    AboutExtraPageComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    SharedModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
