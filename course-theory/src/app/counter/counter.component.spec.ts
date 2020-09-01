f8037cd08b7cd415971da80648101f0b7257fe79
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-01 08:29:42 +0300

    unit tests forms, contorls, toBeTruthy

--- course-theory/src/app/counter/counter.component.spec.ts
+++ course-theory/src/app/counter/counter.component.spec.ts
@@ -1,9 +1,10 @@
 import {CounterComponent} from './counter.component';
+import {FormBuilder} from '@angular/forms';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   beforeEach(() => {
-    component = new CounterComponent();
+    component = new CounterComponent(new FormBuilder());
   });
   //beforeAll, afterEach, afterAll
   it('should increment counter by 1 ',  () => {
@@ -19,5 +20,14 @@ describe('CounterComponent', () => {
     component.counterEmitter.subscribe(v => result = v);
     component.increment();
     expect(result).toBe(1);
+  });
+  it('should create form with two controls',  () => {
+    expect(component.form.contains('login')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+  });
+  it('should mark login as invalid if empty value',  () =>{
+    const control = component.form.get('login');
+    control.setValue('');
+    expect(control.valid).toBeFalsy();
   });
 });
