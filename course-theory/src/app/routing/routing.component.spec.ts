f77a8392c6b0a33cca363e72da735443605138ba
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-02 07:57:01 +0300

    tests  generating

---
+++ course-theory/src/app/routing/routing.component.spec.ts
@@ -0,0 +1,24 @@
+import {ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RoutingComponent } from './routing.component';
+
+describe('RoutingComponent', () => {
+  let component: RoutingComponent;
+  let fixture: ComponentFixture<RoutingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RoutingComponent ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RoutingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+});
