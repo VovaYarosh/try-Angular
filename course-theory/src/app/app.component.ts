99d3f2589efa7d6af7fe7a2ef7bd188688233279
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-03 09:10:56 +0300

    animations events

--- course-theory/src/app/app.component.ts
+++ course-theory/src/app/app.component.ts
@@ -64,4 +64,12 @@ export class AppComponent {
   animate(){
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
   }
+
+  animationStarted(event: AnimationEvent) {
+    console.log('animationStarted', event);
+  }
+
+  animationDone(event: AnimationEvent) {
+    console.log('animationStarted', event);
+  }
 }
