1af84a07fdc86422b2ea52c77a80aaf266df5872
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-05 07:47:21 +0300

    login page p2

--- angular-blog/src/app/admin/login-page/login-page.component.ts
+++ angular-blog/src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -24,7 +25,11 @@ export class LoginPageComponent implements OnInit {
   }
   submit(){
     if (this.form.invalid){
-      return
+      return;
+    }
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
     }
   }
 }
