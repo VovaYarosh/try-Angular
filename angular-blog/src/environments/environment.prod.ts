c06d2266d153143eb850724a3c91060dc9fb1737
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-06 08:40:36 +0300

    create post

--- angular-blog/src/environments/environment.prod.ts
+++ angular-blog/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyBwMGghjTUud5UztKaaIl1MZlVl0iHVrRY'
+  apiKey: 'AIzaSyBwMGghjTUud5UztKaaIl1MZlVl0iHVrRY',
+  fbDbUrl: 'https://angular-blog-f7cd4.firebaseio.com'
 };
--- angular-blog/src/environments/environment.ts
+++ angular-blog/src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBwMGghjTUud5UztKaaIl1MZlVl0iHVrRY'
+  apiKey: 'AIzaSyBwMGghjTUud5UztKaaIl1MZlVl0iHVrRY',
+  fbDbUrl: 'https://angular-blog-f7cd4.firebaseio.com'
 };
 
 /*
