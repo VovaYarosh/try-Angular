773c6d205dfc702ba148b5d6dccb1f9d2a1a4363
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-09 07:42:09 +0300

    typescript basicTypes

---
+++ typescript/basicTypes.js
@@ -0,0 +1,5 @@
+var str = 'hello typescript';
+var num = 42;
+var isActive = false;
+var strArray = ['1', 'd', 'x'];
+var strArray2 = ['1', 'd', 'x'];
---
+++ typescript/basicTypes.ts
@@ -0,0 +1,6 @@
+const str: string = 'hello typescript';
+let num: number = 42;
+let isActive: boolean = false;
+
+let strArray: string[] = ['1','d','x'];
+let strArray2: Array<string> = ['1','d','x'];
