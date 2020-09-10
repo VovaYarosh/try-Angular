40a0ec4ab25b7da948398300634afa68b36fdadc
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-10 07:33:49 +0300

    how RxJS works

---
+++ RxJs/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {interval} from 'rxjs';
+import {} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(){
+    const intervalStream$ = interval(1000);
+    intervalStream$.subscribe((value) => {
+      console.log(value);
+    });
+  }
+}
